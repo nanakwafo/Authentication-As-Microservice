cb01f3b50a7ded3a28e03ec6f1b3026beb7e4adf
Kwafo Nana Mensah (kwafoDEV) <nanamensah11@yahoo.co.uk>
2020-02-03 22:32:04 +0000

    configuring php unit

---
+++ tests/RegistrationTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nanakwafo
+ * Date: 03/02/2020
+ * Time: 21:09
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class RegistrationTest extends TestCase
+{
+
+//    public function testMe()
+//    {
+//
+//        $stack=[];
+//        $this->assertSame(0,count($stack));
+//
+//
+//    }
+}
