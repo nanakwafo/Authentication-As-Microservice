2bd7cd6a8090e7461aee96e661a45eeb7ea98b61
Kwafo Nana Mensah (kwafoDEV) <nanamensah11@yahoo.co.uk>
2020-05-10 22:06:03 +0100

    Update entrypoint.sh

--- docker-compose-deployment/entrypoint.sh
+++ docker-compose-deployment/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-composer config -g github-oauth.github.com de05b52a4a3e9b133cfd93a6e3c33b4dda90ba50
-composer install 
-chmod 755 -R ./
+#composer config -g github-oauth.github.com de05b52a4a3e9b133cfd93a6e3c33b4dda90ba50
+#composer install 
+#chmod 755 -R ./
